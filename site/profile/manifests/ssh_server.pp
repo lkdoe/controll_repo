9a58ea9e7065cef9105bd246c6bdec98db1409a4
lkdoe <82942650+lkdoe@users.noreply.github.com>
2022-03-25 18:16:55 +0100

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
 		ensure => present,
 		user   => 'root',
 		type   => 'ssh-rsa',
-		key    => '',
+		key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQCcmY0O7E3APKq9KtKYelX1hb9P5+qdQiKadhbU2Fp6s0ZlD1NMAH7PAA40lgYdhI7DCNYrQjl1InfQv1cbqclkRepYybO7JRtRETSVETjdmOqmeE2mPLoZYkGmPXbxDkWneQwFQauNqGbbjqIJD3nM5y+LlrCXTID/o6m987OC2nUMxzPrWA/mk2Ftwt4rEK875FF8kn6B0fna7ldroB0p8uqxXpL67v/k0CEKWZ0UdrKqhM8cLoBYTlkcpAicTzU8z/e/4GaYarH4gG0hUf4Ntff2YVkxKw75NBabIuBf+XlHoLByiOr6o+WSo3WLEs6JflU64CXB5Vakoo0GkURn',
 	}  
 }
